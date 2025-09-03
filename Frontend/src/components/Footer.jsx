1c22b170ac314714cbe8c105f8e3609a50c13460
Samapti <samaptigun@gmail.com>
2025-09-03 23:17:39 +0530

    all added

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <div className="item-center justify-center">
         <hr className=" border-black" />
         <h1 className="text-center py-3 text-sm">
-          Copyright © 2024 - All right reserved by ACME Industries Ltd
+          © 2025 - All rights reserved.
         </h1>
       </div>
     </>
